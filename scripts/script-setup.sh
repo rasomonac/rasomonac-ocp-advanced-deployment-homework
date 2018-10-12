3cdb68b976751ea05a28c6402f807b768895ed87
rasomonac <dejan.rasic.ns@gmail.com>
2018-10-12 10:39:31 +0200

    Update and rename script_setup.sh to script-setup.sh

--- scripts/script-setup.sh
+++ scripts/script-setup.sh
@@ -3,7 +3,7 @@
 sed -i -e 's/\r$//' ./scripts/add-users.sh
 sed -i -e 's/\r$//' ./scripts/create_pvs.sh
 sed -i -e 's/\r$//' ./scripts/generate_inventory.sh
-sed -i -e 's/\r$//' ./scripts/multitenancy.sh
+sed -i -e 's/\r$//' ./scripts/multitenancy-test.sh
 sed -i -e 's/\r$//' ./scripts/pvs_large.sh
 sed -i -e 's/\r$//' ./scripts/pvs_small.sh
 sed -i -e 's/\r$//' ./scripts/uninstall.sh
