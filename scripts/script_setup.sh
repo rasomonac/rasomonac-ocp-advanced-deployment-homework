c52b33df6b1175f792fbb1918d9b1b588fb2a941
Dejan Rasic <drasic@tmns.com>
2018-10-12 10:06:24 +0200

    Running script modified.

--- scripts/script_setup.sh
+++ scripts/script_setup.sh
@@ -4,6 +4,6 @@ sed -i -e 's/\r$//' ./scripts/add-users.sh
 sed -i -e 's/\r$//' ./scripts/create_pvs.sh
 sed -i -e 's/\r$//' ./scripts/generate_inventory.sh
 sed -i -e 's/\r$//' ./scripts/multitenancy.sh
-sed -i -e 's/\r$//' ./scripts/pvs_10Gsize.sh
-sed -i -e 's/\r$//' ./scripts/pvs_5Gsize.sh
+sed -i -e 's/\r$//' ./scripts/pvs_large.sh
+sed -i -e 's/\r$//' ./scripts/pvs_small.sh
 sed -i -e 's/\r$//' ./scripts/uninstall.sh
